0f7dc52e45047b8d29eb18b195194ee4f168d83e
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-11 14:13:54 -0400

    added in some routes and created pages folder

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route exact path='/students' element={<Students />} />
+        <Route exact path='/campuses' element={<Campuses />} />
+        <Route path="/campuses/:campusId" Component={SingleCampus} />
+        <Route path="/student/:studentId" Component={SingleStudent} />
+        <Route path='/student' element={<AddStudent />} />
+        <Route path='/campus' element={<AddCampus />} />
+        <Route path="/student/:studentId" element={<EditStudent />} />
+        <Route path="/campus/:campusId" element={<EditCampus />} />
+      </Routes>
+    </Router>
   );
 }
 
