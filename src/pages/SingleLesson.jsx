f85e7cae9714d259565c45e5353b4a0643525bb2
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-13 15:33:46 -0400

    single lesson and lessons is displaying

--- src/pages/SingleLesson.jsx
+++ src/pages/SingleLesson.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-import Navigation from "../components/Navigation";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchSingleLessonThunk } from "../redux/lessons/lessons.actions";
 
 function SingleLesson() {
+  const { lessonId } = useParams();
+  const dispatch = useDispatch();
+  const singleLesson = useSelector((state) => state.lessons.singleLesson);
+
+  useEffect(() => {
+    dispatch(fetchSingleLessonThunk(lessonId));
+  }, [dispatch, lessonId]);
+
   return (
     <div>
-      {/* <Navigation/> */}
-       This is SingleLesson.
+      <h1 className="lesson-title">Lesson</h1>
+
+      {singleLesson ? (
+        <div>
+          <h2 className="Lesson-title">{singleLesson.lessonName}</h2>
+          <p className="paragraph-img">Description: {singleLesson.description}</p>
+          <p className="paragraph-img">Content: {singleLesson.content}
+          </p>
+        </div>
+      ) : (
+        <p className="info-message">No lesson information currently</p>
+      )}
     </div>
   );
 }
