051b494a6752468b2ffe4384f35ffa6c2f7c08b3
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-13 13:07:00 -0400

    pretty much done with lessons i need to fix up the redux files

--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "../components/Navigation";
 import ListingLessons from "../components/ListingLessons";
+import {useSelector } from "react-redux";
 
 function Lessons() {
+  const allLessons = useSelector((state) => state.campuses.allCampuses);
+  useEffect(() => {
+    console.log("FETCH ALL LESSONS FIRING IN USEEFFECT");
+    fetchAllLessons();
+  }, []);
+
+  function fetchAllLessons() {
+    console.log("RUNNING DISPATCH FROM FETCHALLCAMPUSES");
+    return dispatch(fetchAllLessonsThunk());
+  }
+
   return (
     <div>
-      <Navigation/>
-       This is Lessons.
-       <ListingLessons/>
+      <Navigation />
+      <ListingLessons list={allLessons} />
     </div>
   );
 }
