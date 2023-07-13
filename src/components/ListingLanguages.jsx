1462b3291dbc5c5aea34f23d1cebc93003894b65
JiaminShi1226 <shijiamin0324@gmail.com>
2023-07-13 16:26:44 -0400

    Added Language Component and Pages

---
+++ src/components/ListingLanguages.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function ListingLanguages(props) {
+  console.log("LIST LANGUAGES COMPONENT");
+  return props.list.length > 0 ? (
+    props.list.map((item) => {
+      return (
+        <div className="language-grid">
+          <div className="container-lesson" key={item.id}>
+            <div className="div-card-name">
+              <Link className="card-link" to={`/language/${item.id}`}>
+                <h1 className="item-name">{item.languageName}</h1>
+              </Link>
+            </div>
+          </div>
+        </div>
+      );
+    })
+  ) : (
+    <h1 className="info-message">There are no languages here</h1>
+  );
+}
---
+++ src/pages/Languages.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+// import Navigation from "../components/Navigation";
+import ListingLanguages from "../components/ListingLanguages";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllLanguagesThunk } from "../redux/languages/languages.actions";
+
+function Languages() {
+  const allLanguages = useSelector((state) => state.languages.allLanguages);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log("FETCH ALL LESSONS FIRING IN USEEFFECT");
+    fetchAllLanguages();
+  }, []);
+
+  function fetchAllLanguages() {
+    console.log("RUNNING DISPATCH FROM FETCHALLLANGUAGES");
+    return dispatch(fetchAllLanguagesThunk);
+  }
+
+  return (
+    <div>
+      {/* <Navigation /> */}
+      <ListingLanguages list={allLanguages} />
+    </div>
+  );
+}
+
+export default Languages;
