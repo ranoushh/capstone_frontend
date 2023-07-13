da1d7d7569085b23d731d6f77c4641c9a3570341
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-13 11:12:51 -0400

    added in naviagation to all pages

--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navigation from "../components/Navigation";
 
 function Lessons() {
   return (
     <div>
+      <Navigation/>
        This is Lessons.
     </div>
   );
--- src/pages/SingleLesson.jsx
+++ src/pages/SingleLesson.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navigation from "../components/Navigation";
 
 function SingleLesson() {
   return (
     <div>
+      <Navigation/>
        This is SingleLesson.
     </div>
   );
