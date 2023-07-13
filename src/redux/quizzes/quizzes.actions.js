422c7e5870b94e1a65be021a9137898be4821c2e
Aminata <aminata.cisse44@bcmail.cuny.edu>
2023-07-13 16:07:03 -0400

    redux done for tests & quizzes

---
+++ src/redux/quizzes/quizzes.actions.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import QuizzesActionType from "./quizzes.types";
+
+// Fetching all quizzes
+export const fetchAllQuizzes = (payload) => {
+  console.log("FETCH ALL quizzes ACTION");
+  return {
+    type: QuizzesActionType.FETCH_ALL_QUIZZES,
+    payload: payload,
+  };
+};
+
+export const fetchAllQuizzesThunk = () => {
+  return async (dispatch) => {
+    try {
+      console.log("FETCH_ALL_QUIZZES_THUNK is firing");
+      const response = await axios.get("http://localhost:8080/api/quizzes");
+      console.log("FETCH_ALL_QUIZZES_THUNK completed");
+      dispatch(fetchAllQuizzes(response.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+// Fetching a single quiz
+export const fetchSingleQuiz = (payload) => {
+  console.log("FETCH SINGLE QUIZ ACTION");
+  return {
+    type: QuizzesActionType.FETCH_SINGLE_QUIZ,
+    payload: payload,
+  };
+};
+
+export const fetchSingleQuizThunk = (id) => {
+  return async (dispatch) => {
+    try {
+      console.log("FETCH_SINGLE_quiz_THUNK is firing");
+      const response = await axios.get(`http://localhost:8080/api/quizzes/${id}`);
+      console.log("FETCH_SINGLE_QUIZ_THUNK completed");
+      dispatch(fetchSingleQuiz(response.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
