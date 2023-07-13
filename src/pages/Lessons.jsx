e78b384fbfb5b965f8538534ac1ed1d4d483a607
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-13 11:19:20 -0400

    added listing lessons, will work on that

--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import ListingLessons from "../components/ListingLessons";
 
 function Lessons() {
   return (
     <div>
       <Navigation/>
        This is Lessons.
+       <ListingLessons/>
     </div>
   );
 }
