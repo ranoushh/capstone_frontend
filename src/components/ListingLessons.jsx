1f9d815ddceeb3f37051be745cee06322b99c6a4
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-13 13:07:00 -0400

    pretty much done with lessons i need to fix up the redux files

--- src/components/ListingLessons.jsx
+++ src/components/ListingLessons.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-class ListingLessons{
-
+export default function ListingLessons() {
+    console.log("LIST LESSONS COMPONENT");
+    return props.list.length > 0 ? (
+        props.list.map((item) => {
+            return (
+                <div className="campus-grid">
+                    <div className="container-campus" key={item.id}>
+                        <div className="campus-pic">
+                            <img src={item.imageUrl} alt={item.name} />
+                        </div>
+                        <div className="div-card-name">
+                            <Link className="card-link" to={`/campuses/${item.id}`}>
+                                <h1 className="item-name">{item.name}</h1>
+                            </Link>
+                        </div>
+                        <button onClick={() => props.handleRemoveCampus(item.id)}>X</button>
+                    </div>
+                </div>
+            );
+        })
+    ) : (
+        <h1 className="info-message">There are no campuses registered</h1>
+    );
 }
-
-export default ListingLessons;
