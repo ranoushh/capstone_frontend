7ea25271fe362ba812d77622b7cae76ab6914d21
rana <abedorana7@gmail.com>
2023-07-17 13:39:13 -0400

    [feature] setup chat connection with backend, setup chat component

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Navigation from "../components/Navigation";
 
+
+//idea is that users can join a room by their roomid
 function Chat() {
-    console.log("chat")
+  const [user, setUser] = useState();
+  const [room, setRoom] = useState();
+
+
+  function joinRoom(){
+    
+  };
+
   return (
     <div>
-      {/* <Navigation/> */}
-       This is Chat.
-        <li>
-            <Link to="/">HOME</Link>
-        </li>
+
+        <h4>Start Chatting!</h4>
+        <input type="text" placeholder="Name" onChange={(event) => {setUser(event.target.value)}}/>
+        <input type="text" placeholder="Room ID" onChange={(event) => {setRoom(event.target.value)}}/>
+        <button onSubmit= {joinRoom}>Join</button>
     </div>
   );
 }
