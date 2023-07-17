0e5644374d34246a746d1d47ce79dd99b7b00c39
JiaminShi1226 <shijiamin0324@gmail.com>
2023-07-17 11:02:10 -0400

    Connect quizQuestions to single quiz page

--- src/pages/SingleQuiz.jsx
+++ src/pages/SingleQuiz.jsx
@@ -2,14 +2,20 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchSingleQuizThunk } from "../redux/quizzes/quizzes.actions";
+import ListingQuizQuestion from "../components/ListingQuizQuestion";
+import { fetchAllQuizQuestionThunk } from "../redux/quizQuestion/quizQuestion.actions";
 
 function SingleQuiz() {
   const { quizId } = useParams();
   const dispatch = useDispatch();
   const singleQuiz = useSelector((state) => state.quizzes.singleQuiz);
+  const quizQuestion = useSelector(
+    (state) => state.quizQuestion.allQuizQuestion
+  );
 
   useEffect(() => {
     dispatch(fetchSingleQuizThunk(quizId));
+    dispatch(fetchAllQuizQuestionThunk());
   }, [dispatch, quizId]);
 
   return (
@@ -20,6 +26,7 @@ function SingleQuiz() {
         <div>
           <h2 className="quiz-title">{singleQuiz.quizName}</h2>
           <p className="paragraph-img">Difficulty: {singleQuiz.difficulty}</p>
+          <ListingQuizQuestion list={quizQuestion} quizId={singleQuiz.id} />
         </div>
       ) : (
         <p className="info-message">No quiz information currently</p>
---
+++ src/redux/quizQuestion/quizQuestion.actions.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import QuizQuestionActionType from "./quizQuestion.types";
+
+// Fetching all quizQuestion
+export const fetchAllQuizQuestion = (payload) => {
+  console.log("FETCH ALL QUIZQUESTION ACTION");
+  return {
+    type: QuizQuestionActionType.FETCH_ALL_QUIZQUESTION,
+    payload: payload,
+  };
+};
+
+export const fetchAllQuizQuestionThunk = () => {
+  return async (dispatch) => {
+    try {
+      console.log("FETCH_ALL_QUIZQUESTION_THUNK is firing");
+      const response = await axios.get("http://localhost:8080/api/quizQuestion");
+      console.log("FETCH_ALL_QUIZQUESTION_THUNK completed");
+      dispatch(fetchAllQuizQuestion(response.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+// Fetching a single quizQuestion
+export const fetchSingleQuizQuestion = (payload) => {
+  console.log("FETCH SINGLE QUIZQUESTION ACTION");
+  return {
+    type: QuizQuestionActionType.FETCH_SINGLE_QUIZQUESTION,
+    payload: payload,
+  };
+};
+
+export const fetchSingleQuizQuestionThunk = (id) => {
+  return async (dispatch) => {
+    try {
+      console.log("FETCH_SINGLE_QUIZQUESTION_THUNK is firing");
+      const response = await axios.get(`http://localhost:8080/api/quizQuestion/${id}`);
+      console.log("FETCH_SINGLE_QUIZQUESTION_THUNK completed");
+      dispatch(fetchSingleQuizQuestion(response.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
