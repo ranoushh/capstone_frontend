e06b8f6de84aad4c772763509524bc497e4cf3e8
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-18 15:20:24 -0400

    leaderboard somewhat works but it's not receiving user info

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"; // Import useDispatch
 import Navigation from "../components/Navigation";
 import { fetchAllUsersThunk } from "../redux/users/users.actions";
-import ListingUsers from "../components/ListingUsers";
 
 function Leaderboard() {
   const allUsers = useSelector((state) => state.users.allUsers);
   const dispatch = useDispatch(); // Add useDispatch here
+  console.log((useSelector(state => state)));
 
   function fetchAllUsers() {
     console.log("RUNNING DISPATCH FROM FETCHALLAchievements");
@@ -17,11 +17,17 @@ function Leaderboard() {
     console.log("FETCH ALL USERS FIRING IN USEEFFECT");
     fetchAllUsers();
   }, []);
-  
+
   return (
     <div>
-      <h1 className="title">Leaderboard</h1>
-      <ListingUsers list={allUsers}/>
+      <h1 className="title">Leaderboard </h1>
+      <ul>
+        {allUsers && allUsers.length > 0 ?
+          (allUsers.map((item, index) => (
+            <li key={index}>{item.email}</li>
+          ))) : (<h2 className="info-message">There are no users here</h2>)
+        };
+      </ul>
     </div>
   );
 }
