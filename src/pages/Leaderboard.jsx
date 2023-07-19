f16370a07cf6d9db33057792f704a371a3b9dd8b
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-19 12:09:11 -0400

    fixed the css cuz some other files clashed

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"; // Import useDispatch
+import { useDispatch, useSelector } from "react-redux";
 import Navigation from "../components/Navigation";
 import { fetchAllUsersThunk } from "../redux/users/users.actions";
 
 function Leaderboard() {
   const allUsers = useSelector((state) => state.users.allUsers);
-  const dispatch = useDispatch(); // Add useDispatch here
-  console.log((useSelector(state => state)));
+  const dispatch = useDispatch();
 
   function fetchAllUsers() {
     console.log("RUNNING DISPATCH FROM FETCHALLAchievements");
@@ -20,13 +19,13 @@ function Leaderboard() {
 
   return (
     <div>
-      <h1 className="title">Leaderboard </h1>
+      <h1 className="title">Leaderboard</h1>
       <ul>
-        {allUsers && allUsers.length > 0 ?
-          (allUsers.map((item, index) => (
-            <li key={index}>{item.email}</li>
-          ))) : (<h2 className="info-message">There are no users here</h2>)
-        };
+        {allUsers && allUsers.length > 0 ? (
+          allUsers.map((item, index) => <li key={index}>{item.email}</li>)
+        ) : (
+          <h2 className="info-message">There are no users here</h2>
+        )}
       </ul>
     </div>
   );
