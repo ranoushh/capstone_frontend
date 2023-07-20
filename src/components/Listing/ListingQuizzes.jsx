adedb35e21d22bdf3211427fac1adfa772ca7ed1
JiaminShi1226 <shijiamin0324@gmail.com>
2023-07-20 19:53:32 -0400

    Fixed Wrong paths

--- src/components/Listing/ListingQuizzes.jsx
+++ src/components/Listing/ListingQuizzes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../style/singleLanguage.css";
+import "../../style/singleLanguage.css";
 import { useParams } from "react-router";
 import { useDispatch } from "react-redux";
 import { deleteQuizThunk } from "../../redux/quizzes/quizzes.actions";
