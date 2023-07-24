23e6a49e92a3ec9ed50b821f992f91dcd9a34af9
Aminata <aminata.cisse44@bcmail.cuny.edu>
2023-07-24 09:27:03 -0400

    [feature] freindship models fix ip

--- src/pages/Add/AddFriend.jsx
+++ src/pages/Add/AddFriend.jsx
@@ -1,27 +1,71 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function AddFriend() {
+  const [searchInput, setSearchInput] = useState("");
+  const [userList, setUserList] = useState([]);
 
-//fetch all users from our database
-const [searchInput, setSearchInput] = useState("");
+  function handleSearch() {
+    // Make an API request to fetch users based on searchInput
+    fetch(`/users?search=${searchInput}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Update the userList state with the fetched users
+        setUserList(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }
 
-function handleSearch(){
-    //if user exists, list that user and have add button etc 
-    //if user doesn't, say user not found
-   
-};
+  function sendFriendRequest(userId) {
+    // Make an API request to send a friend request to the user with the given userId
+    fetch("/addfriend", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Friend request sent:", data);
+        // Optionally, you can update the user interface to indicate that the friend request was sent successfully.
+      })
+      .catch((error) => {
+        console.error("Error sending friend request:", error);
+      });
+  }
 
   return (
     <div>
-        <input 
-        type="text" 
+      <input
+        type="text"
         placeholder="Search friend's user"
+        value={searchInput}
         onChange={(e) => setSearchInput(e.target.value)}
-        ></input>
-        <button onClick={handleSearch} >Search</button>
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      {/* Display the list of users */}
+      {userList.length > 0 && (
+        <div>
+          <h2>Search Results:</h2>
+          <ul>
+            {userList.map((user) => (
+              <li key={user.id}>
+                {user.username} {/* You can display any relevant user information */}
+                {/* Add a button to send friend request */}
+                <button onClick={() => sendFriendRequest(user.id)}>Add Friend</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {/* Display "User not found" message if no results */}
+      {userList.length === 0 && searchInput !== "" && <div>User not found.</div>}
     </div>
-  )
-};
+  );
+}
 
 export default AddFriend;
