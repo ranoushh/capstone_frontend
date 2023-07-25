bc3603161ef9f7b8bc39e6bc619320ae0049ebe2
rana <abedorana7@gmail.com>
2023-07-25 13:54:27 -0400

    [feature] add friend redux & setup add friend page

--- src/pages/Add/AddFriend.jsx
+++ src/pages/Add/AddFriend.jsx
@@ -1,31 +1,61 @@
-import React from 'react'
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
+import React from "react";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAllUsersThunk, fetchFriendsThunk } from "../../redux/usersCrud/users.actions";
+import { me } from "../../redux/user";
 
 function AddFriend() {
   const user = useSelector((state) => state.user);
   const friends = useSelector((state) => state.usersCrud.friends);
-  const dispatch = useDispatch();
+  const allUsers = useSelector((state) => state.usersCrud.allUsers);
   const [searchInput, setSearchInput] = useState("");
+  const [findingUsers, setFindingUsers] = useState([]);
+  const dispatch = useDispatch();
 
-  function handleSearch(){
-      //if user exists, list that user and have add button etc 
-      //if user doesn't, say user not found
-    
-  };
+  async function fetchAllData() {
+    try {
+      await dispatch(me());
+      await dispatch(fetchAllUsersThunk());
+      // await dispatch(fetchFriendsThunk(user.id));
+    } catch (error) {
+      console.log("error fetching data " + error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAllData();
+  }, [dispatch, user.id]);
+
+  function handleSearch() {
+    //if user exists, list that user and have add button etc
+    //if user doesn't, say user not found
+
+    setFindingUsers(
+      allUsers.filter(
+        (user) => user.username === searchInput
+      )
+    );
+
+    console.log("users found: " + findingUsers)
+  }
+
   
-
   return (
     <div>
-        <input 
-        type="text" 
+      <input
+        type="text"
         placeholder="Search friend's user"
-        onChange={(e) => setSearchInput(e.target.value)}
-        ></input>
-        <button onClick={handleSearch} >Search</button>
+        onChange={(e) => setSearchInput(e.target.value.toLowerCase())}
+      ></input>
+      <button onClick={handleSearch}>Search</button>
+
+      {findingUsers.length > 0 ? (
+        findingUsers.map((item) => <li key={item.id}>{item.username}</li>)
+      ) : (
+        <p>No User Found</p>
+      )}
     </div>
-  )
-};
+  );
+}
 
 export default AddFriend;
