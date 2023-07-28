f97fe6e3f08d2df31efa81c2c9e1ef4137e49d95
Aminata <aminata.cisse44@bcmail.cuny.edu>
2023-07-28 01:57:30 -0400

    [feature] Protected Routes fix IP

--- src/Utils/Auth.js
+++ src/Utils/Auth.js
@@ -1,41 +1,41 @@
-import { useEffect,useState } from "react";
-import { useDispatch } from "react-redux";
-import { Outlet, Navigate } from "react-router-dom";
-import { me } from "../redux/user";
+// import { useEffect,useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { Outlet, Navigate } from "react-router-dom";
+// import { me } from "../redux/user";
 
-export default function ProtectedRoute({ isLoggedIn }) {
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(me());
-  // }, [dispatch]);
+// export default function ProtectedRoute({ isLoggedIn }) {
+//   // const dispatch = useDispatch();
+//   // useEffect(() => {
+//   //   dispatch(me());
+//   // }, [dispatch]);
 
-  // const [loading, setLoading] = useState(true);
-  // const [loggedIn, setLoggedIn] = useState(false);
+//   // const [loading, setLoading] = useState(true);
+//   // const [loggedIn, setLoggedIn] = useState(false);
 
-  // useEffect(() => {
-  //   let mounted = true;
-  //   const loggedInStatus = async () => {
-  //     const result = await isLoggedIn()
-  //     // if(mounted) {
-  //     //   setLoggedIn(result);
-  //     //   setLoading(false);
-  //     // }
-  //   }
-  //   loggedInStatus();
-  //   return () => {mounted= false;}
+//   // useEffect(() => {
+//   //   let mounted = true;
+//   //   const loggedInStatus = async () => {
+//   //     const result = await isLoggedIn()
+//   //     // if(mounted) {
+//   //     //   setLoggedIn(result);
+//   //     //   setLoading(false);
+//   //     // }
+//   //   }
+//   //   loggedInStatus();
+//   //   return () => {mounted= false;}
 
-  // }
-  // , [isLoggedIn]
-  // )
-  // console.log("protected is logged in ", isLoggedIn);
-  // if(loading){
-  //   console.log("if true reached");
-  //  return <div> isLoading </div>
-  // }
+//   // }
+//   // , [isLoggedIn]
+//   // )
+//   // console.log("protected is logged in ", isLoggedIn);
+//   // if(loading){
+//   //   console.log("if true reached");
+//   //  return <div> isLoading </div>
+//   // }
   
-  // }else{
-  //   console.log("else false reached")
-  //   return <Navigate to="/login" />
-  // }
-  return isLoggedIn ? <Outlet /> : <Navigate to="/signup" />;
-}
+//   // }else{
+//   //   console.log("else false reached")
+//   //   return <Navigate to="/login" />
+//   // }
+//   return isLoggedIn ? <Outlet /> : <Navigate to="/signup" />;
+// }
